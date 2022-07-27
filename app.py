fb82c68f0394d51df58812b80e6b80440369dd93
pluis9 <luispalcivar@gmail.com>
2022-07-26 22:29:23 -0500

    login funcional

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
 import os
-from flask import Flask, jsonify, make_response, render_template, request, flash, url_for, redirect
+from flask import Flask, jsonify, make_response, render_template, request, flash, url_for, redirect, session
 import cryptocode
 import json
 import pymongo
+import bcrypt
 
 
 #conexion base datos
-myClient=pymongo.MongoClient("mongodb://localhost:27017")
-myDb=myClient["taskapp"]
+
+MONGO_HOST="localhost"
+MONGO_PUERTO="27017"
+MONGO_TIEMPO_FUERA=1000
+
+MONGO_URI="mongodb://"+MONGO_HOST+":"+MONGO_PUERTO+"/"
+
+MONGO_BASEDATOS="preescolar"
+MONGO_COLECCION="usuariosDocentes"
+cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMS=MONGO_TIEMPO_FUERA)
+#base de datos
+baseDatos=cliente[MONGO_BASEDATOS]
+#colección
+coleccion=baseDatos[MONGO_COLECCION]
+#Encuentra el primer documento
+x=coleccion.find_one()
+print(x)
+#Retorna todos los documentos de la coleccion
+for documento in coleccion.find():
+    print(documento)
 
 #instancia de la aplicación
 app = Flask(__name__)
-
+#clave secreta de la aplicación
+app.secret_key = "luisparedez"
 #rutas de la carpeta templates/static
 app._static_folder = os.path.abspath("templates/static")
 
@@ -40,12 +60,46 @@ def accederalumno():
     """Retorna Login de niños"""
     return render_template("layouts/login.html")
 
-
 @app.route("/logindocente.html")
 
 def logindocente():
     """Retorna Login de docentes"""
+
+
     return render_template("layouts/logindocente.html")
+
+
+
+@app.route("/logindocente1", methods=['POST'])
+
+def logindocente1():
+        "Retorna Login de docentes"
+
+        login_user = coleccion.find_one({'correo' : request.form['correo']})
+
+        if login_user:
+            if bcrypt.hashpw(request.form['contrasenia'].encode('utf-8'), login_user['contrasenia'].encode('utf-8')) == login_user['contrasenia'].encode('utf-8'):
+                session['correo'] = request.form['correo']
+                return index()
+
+        return 'Invalid username/password combination'
+
+@app.route('/registroDocente', methods=['POST', 'GET'])
+def registroDocente():
+    if request.method == 'POST':
+        c=coleccion
+        existing_user =  c.find_one({'correo' : request.form['correo']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['contrasenia'].encode('utf-8'), bcrypt.gensalt())
+            c.insert_one({'correo' : request.form['correo'], 'contrasenia' : hashpass})
+            session['correo'] = request.form['correo']
+            return index()
+        
+        return 'That username already exists!'
+
+    return render_template('layouts/registrodocente.html')
+
 
 
 
@@ -53,93 +107,6 @@ def logindocente():
 
 
 
-diccionario_Docentes={}
-@app.route('/nuevoregistrodocente',methods=['POST'])
-
-
-def nuevoregistrodocente(): 
-    '''función que permite guardar los datos de un nuevo docente'''
-      #con path se especifica la ruta del archivo
-    path, _=os.path.split(os.path.abspath(__file__))
-    
-       
-    with open(path+'/registrodocente.json') as file:
-        data_Docentes= json.load(file)
-
-    nombreDocente= request.form.get('nombre')
-    telefonoDocente= request.form.get('telefono')
-    experienciaDocente= request.form.get('experiencia')
-    correoDocente= request.form.get('correo')
-    contraseniaDocente= request.form.get('contrasenia')
-   
-
-
-    #cifrado de la contraseña
-    cifrado=cryptocode.encrypt(contraseniaDocente, "password")
-
-    #validar que se ingresen datos
-    if len(nombreDocente)>0 and len(telefonoDocente)>0 and len(experienciaDocente)>0 and len(correoDocente)>0 and len(contraseniaDocente)>0:
-       
-       
-      
-       
-        diccionario_Docentes={'nombreDocente':nombreDocente, 'telefonoDocente':telefonoDocente,
-        'experienciaDocente':experienciaDocente, 'correoDocente': correoDocente, 'contraseniaDocente':cifrado}
-        
-        data_Docentes.append(diccionario_Docentes)
-        
- 
-    
-         #se escribe en el archivo datos.json
-        with open(path+f'/registroDocente.json','w') as file:
-            json.dump(data_Docentes, file, indent=4)
-
-     
-    else:
-        #alerta en caso de que existan errores
-        flash('Error al ingresar los datos')
-        return render_template('registroDocente.html')
-
-#Validación de usuarios Docentes
-
-#ruta hacia validar_usuarios_Docentes
-@app.route('/validar_usuarios_Docentes',methods=['POST'])
-
-#función que permite visualizar las tareas almacenadas
-def validar_usuarios_Docentes():
-    if request.method=='POST':
-        correo= request.form.get("correo")
-        contrasenia= request.form.get("contrasenia")
-
-        path, _=os.path.split(os.path.abspath(__file__))
-        
-    #se carga el archivo registropoatulanye.json
-        with open(path+'/registroDocente.json') as file:
-            data= json.loads(file.read())
-        
-        #se recorre cada elemento del diccionario
-        for elemento in range(len(data)):
-            
-            try:
-
-            #descifrado de la contraseña
-                descifrado= cryptocode.decrypt(data[elemento]["contraseniaPostulante"], 'password')
-                if correo!=data[elemento]["correoPostulante"] and contrasenia!=data[elemento][descifrado]:
-                    elemento=elemento+1
-                    #retorna nuevamente la misma página
-                    flash(data[elemento]["correoPostulante"])
-                    return render_template('logindocente.html')
-                else:
-                    #si se valida retorna la página de aprendizale
-                     return index()
-                    
-                    
-            except KeyError:
-                continue
-    else:
-        flash("Error")
-        return render_template('layouts/logindocente.html')
-    return  render_template('layouts/logindocente.html')
 
 @app.route('/validar_usuario_Alumno',methods=['POST'])
 def loginAlumno():
